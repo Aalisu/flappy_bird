332aebf83bc21a4a8b12b626b8c49615ebe77d4f
Aalisu <aalialsulaimani@gmail.com>
2023-05-25 12:40:52 +0800

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+              flex: 3,
+              child: Container(
+                color: Colors.blue,
+              )),
+          Expanded(
+              child: Container(
+            color: Colors.green,
+          ))
+        ],
+      ),
+    );
+  }
+}
